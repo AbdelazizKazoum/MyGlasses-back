a8bbe099e0cfb1477880d3807dc63dfba5db7f98
Abdelaziz_kazoum <abdukazoum@gmail.com>
2025-02-24 10:04:03 +0000

    fix env

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -18,7 +18,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT || 5000),
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'], // Add your frontend domain here
+    origin: [process.env.ORIGIN_CLIENT], // Add your frontend domain here
     credentials: true, // Allow cookies to be sent with requests
   });
   app.use(cookieParser());
